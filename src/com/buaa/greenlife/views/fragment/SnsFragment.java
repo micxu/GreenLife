d8a1aabdb701e0f92b02bb613b95ef68b21d5970
QisenTang <woaitqs@gmail.com>
2013-07-21 03:02:03 +0800

    add async-httpclient

--- src/com/buaa/greenlife/views/fragment/SnsFragment.java
+++ src/com/buaa/greenlife/views/fragment/SnsFragment.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.os.Handler;
 
 import com.buaa.greenlife.R;
+import com.loopj.android.http.AsyncHttpClient;
 
 /**
  * Created by QisenTang on 13-7-20.
  */
 public class SnsFragment extends BaseFragment {
+
+    AsyncHttpClient httpClient = new AsyncHttpClient();
+
     public SnsFragment(Context context, Handler handler) {
         super(context, handler);
     }

c1eb1156de352ef05d2c76f18512ee9c256b75c0
liuyuxiao <liuyuxiao123@gmail.com>
2013-07-20 14:22:05 +0800

    liuyuxiao first commit

---
+++ src/com/buaa/greenlife/ShareActivity.java
@@ -0,0 +1,140 @@
+package com.buaa.greenlife;
+
+import com.baidu.social.core.BaiduSocialException;
+import com.baidu.social.core.BaiduSocialListener;
+import com.baidu.social.core.Utility;
+import com.baidu.sociallogin.BaiduSocialLogin;
+import com.buaa.configs.BaiduSocialShareConfig;
+
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.app.Activity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class ShareActivity extends Activity {
+
+	final Handler handler = new Handler(Looper.getMainLooper());
+
+	private BaiduSocialLogin socialLogin;
+
+	private final static String appKey = BaiduSocialShareConfig.mbApiKey;
+
+	private ImageButton sinaWeibo;
+
+	private ImageButton qqzone;
+
+	private ImageButton qqWeibo;
+
+	private ImageButton kaixin;
+	
+	private ImageButton renren;
+
+	private Button clean;
+	
+	private ImageButton baidu;
+
+	private EditText info;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().setSoftInputMode(
+				WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+		setContentView(R.layout.activity_share);
+
+		// 实例化baidu社会化登录，传入appkey
+		socialLogin = BaiduSocialLogin.getInstance(this, appKey);
+
+		// 设置支持腾讯微博单点登录的appid
+		socialLogin.supportQQSso(BaiduSocialShareConfig.QQ_SSO_APP_KEY);
+
+		// 设置支持新浪微博单点登录的appid
+		socialLogin.supportWeiBoSso(BaiduSocialShareConfig.SINA_SSO_APP_KEY);
+
+		sinaWeibo = (ImageButton) findViewById(R.id.imageButton1);
+		qqzone = (ImageButton) findViewById(R.id.imageButton2);
+		qqWeibo = (ImageButton) findViewById(R.id.imageButton3);
+		kaixin = (ImageButton) findViewById(R.id.imageButton4);
+		renren=(ImageButton)findViewById(R.id.imageButton5);
+		
+		
+		clean = (Button) findViewById(R.id.button1);
+
+		baidu=(ImageButton)findViewById(R.id.imageButton6);
+		
+		clean.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				socialLogin.cleanAllAccessToken();
+			}
+		});
+
+		sinaWeibo.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+
+				if (socialLogin
+						.isAccessTokenValid(Utility.SHARE_TYPE_SINA_WEIBO)) {
+					socialLogin.getUserInfoWithShareType(ShareActivity.this,
+							Utility.SHARE_TYPE_SINA_WEIBO,
+							new UserInfoListener());
+				} else
+					socialLogin.authorize(ShareActivity.this,
+							Utility.SHARE_TYPE_SINA_WEIBO,
+							new UserInfoListener());
+
+			}
+		});
+
+		
+	}
+	
+	
+	class UserInfoListener implements BaiduSocialListener {
+
+		@Override
+		public void onAuthComplete(Bundle values) {
+			// TODO Auto-generated method stubis
+		}
+
+		@Override
+		public void onApiComplete(String responses) {
+			// TODO Auto-generated method stub
+			final String responseStr = responses;
+			handler.post(new Runnable() {
+				@Override
+				public void run() {
+					info = (EditText) findViewById(R.id.editText1);
+					info.setText(Utility.decodeUnicode(responseStr));
+				}
+			});
+		}
+
+		@Override
+		public void onError(BaiduSocialException e) {
+			final String error = e.toString();
+			handler.post(new Runnable() {
+				@Override
+				public void run() {
+					info = (EditText) findViewById(R.id.editText1);
+					info.setText(error);
+				}
+			});
+		}
+	}
+}
